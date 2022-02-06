3dcbaf7e26f1e2c980edf4f229cff912e4e15fc1
FatGrizzly <66728880+FatGrizzly@users.noreply.github.com>
2022-02-06 19:21:23 +0530

    assume yes on fedora based systems

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ if [ `which yum` ]; then
   if ! command -v node -v &> /dev/null
   then
     curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-    yum install nodejs
+    yum -y install nodejs
   fi
 elif [ `which apt` ]; then
   if ! command -v node -v &> /dev/null
